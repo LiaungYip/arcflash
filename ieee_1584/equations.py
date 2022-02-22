40cd09eedbd07e94fc3ce857b13f8d07c4b99dd1
Li-aung Yip <liaung.yip@ieee.org>
2022-02-22 22:00:46 +0800

    Replace complicated (official) equations for arc flash boundary with simplified equations, derived from algebraic manipulation of Eq 3 and Eq 7.

--- ieee_1584/equations.py
+++ ieee_1584/equations.py
@@ -93,16 +93,91 @@ def E_AFB_intermediate(c: Cubicle, V_oc: float, I_arc: float, I_bf: float, T: fl
     # Equations 3, 4, 5, 6
     E = x1 * 10 ** (x2 + x3 + x4 + x5)
 
+    # # Equations 7, 8, 9, 10
+    # # The official equations
+    # x6_num = x2 + x3 + x4 - log10(20 / T)
+    # x6_den = - k["k12"]  # note -ve sign
+    # x6 = x6_num / x6_den
+    # AFB = 10 ** x6
+
     # Equations 7, 8, 9, 10
-    x6_num = x2 + x3 + x4 - log10(20 / T)
-    x6_den = - k["k12"]  # note -ve sign
-    x6 = x6_num / x6_den
-    AFB = 10 ** x6
+    # The simplified equations
+    AFB = intermediate_AFB_from_E(c, V_oc, E)
 
     assert E >= 0
     assert AFB >= 0
 
     return E, AFB
+
+
+def intermediate_AFB_from_E(c: Cubicle, V_oc: float, E: float):
+    # Calculates the (intermediate) arc flash boundary, i.e. AFB_600, from the incident energy i.e. E_600 only.
+    # Knowledge of T, G, I_arc, I_bf, and CF is not required.
+    # This is useful for multi-time-step calculations where there is no singular value of T, I_arc, or I_bf.
+    #
+    # Explanation:
+    # ============
+    #
+    # The IEEE 1584-2018 formulas for arc flash boundary (AFB), i.e. eq's 7, 8, 9, and 10, are pretty complicated.
+    #
+    # In particular, the equations for AFB requires knowledge of time T, busbar gap G, the currents I_arc and I_bf,
+    # and size correction factor CF.
+    #
+    # This is a problem when doing multi-time-step arc flash calculations where the values of T, I_arc, and I_bf are
+    # different for each time-step. What single value of I_arc would you plug into Eq 7, when I_arc is 10 kA for 100 ms,
+    # then 5 kA for 900 ms, then 2 kA for 1,000 ms?
+    #
+    # Consider Eq 3 for the quantity E_600.
+    #
+    # First, we recognise that the relationship between incident energy E_600 (J/cm²) and distance D (mm) is simply
+    # that __the energy E_600 falls off exponentially with distance D__. If we rearrange Eq 3 using exponent identities
+    # we can simplify to:
+    #
+    #       E_600 = F * ( D ^ k12 )
+    #
+    # Where:
+    #   * E_600 is the __intermediate__ arcing energy at V = 0.6 kV, with units of J/cm²,
+    #   * F_600 is a (reasonably complicated) function of time T, busbar gap G, currents I_arc and I_bf, and size
+    #     correction factor CF,
+    #   * D is the distance (mm) from the fault to where E_600 has been measured.
+    #   * k12 is a constant __"distance exponent"__ from Table 3, Table 4, or Table 5.
+    #
+    # We can calculate what F_600** would have been:
+    #
+    #       F_600 = E_600 / ( D ^ k12 )
+    #
+    # Once we know the value of F_600, we can calculate E_600' at any distance D' we like:
+    #
+    #       E_600' (at distance D') = F_600 * ( D' ^ k12 )
+    #
+    # Alternately, we can calculate the distance D' that will give a particular value of E_600'.
+    #
+    #       D' = (E_600' / F_600) ^ ( 1 / k12 )
+    #
+    # Finally note that the arc flash boundary, AFB_600, is simply the special case where E_600' is equal to exactly
+    # 1.2 cal/cm². Noting that 1.2 cal/cm² * 4.184 J/cal = 5.0208 J/cm²,
+    #
+    #       AFB_600 = (5.0208 / F_600) ^ ( 1 / k12 )
+    #
+    # ** Sidenote: The physical meaning of the quantity "F_600" is that F_600 is, in some sense, the total amount of
+    # energy released (i.e. Joules). (The distribution of energy is not isotropic, i.e. k12 != -2.00, so this
+    # interpretation is not exact.)
+
+    assert (V_oc <= 0.6) or (V_oc in (0.6, 2.7, 14.3,))
+
+    if V_oc <= 0.6:
+        k = table_3[c.EC]
+    elif V_oc == 2.7:
+        k = table_4[c.EC]
+    elif V_oc == 14.3:
+        k = table_5[c.EC]
+    else:
+        k = None
+
+    # After all the explanation, calculation of the (intermediate) AFB is simply 2 lines.
+    F = E / (c.D ** k["k12"])
+    AFB = (5.0208 / F) ** (1 / k["k12"])
+    return AFB
 
 
 def interpolate(c: Cubicle, x_600, x_2700, x_14300):
